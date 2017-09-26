a8e2030bec811e96745177dc5c96ff0bbb47a983
Tautvydas <tutis.navickas@mail.utoronto.ca>
2017-09-26 19:37:24 -0400

    Initial commit with starting code for map generation

---
+++ BassDefense/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    public Transform tilePrefab;
+    
+    [Range(0,100)]
+    public int width;
+    [Range(0, 100)]
+    public int height;
+
+    int[,] map;
+
+    void Start() {
+        GenerateMap();
+    }
+
+    public void GenerateMap() {
+        map = new int[width, height];
+    }
+}
