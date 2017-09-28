58ef590def367dfdacd5eaa56e6a88ca07732610
Tautvydas <tutis.navickas@mail.utoronto.ca>
2017-09-28 00:08:24 -0400

    Implemented map generation from image file

--- BassDefense/Assets/Scripts/MapGenerator.cs
+++ BassDefense/Assets/Scripts/MapGenerator.cs
@@ -3,22 +3,61 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 
+[System.Serializable]
+public class TileSprite {
+    public Color32 color;
+    public GameObject[] spriteArray;
+}
+
 public class MapGenerator : MonoBehaviour {
+    public Texture2D mapSprite;
+    public TileSprite[] tileSprites;
 
-    public Transform tilePrefab;
-    
-    [Range(0,100)]
+    [Range(1,100)]
     public int width;
-    [Range(0, 100)]
+    [Range(1, 100)]
     public int height;
-
-    int[,] map;
+    Dictionary<Color32, GameObject[]> spriteDictionary;
 
     void Start() {
+        if(mapSprite != null && tileSprites != null) {
+            width = mapSprite.width;
+            height = mapSprite.height;
+
+            spriteDictionary = new Dictionary<Color32, GameObject[]>();
+            foreach(TileSprite tileSprite in tileSprites) {
+                spriteDictionary.Add(tileSprite.color, tileSprite.spriteArray);
+            }
+        }
         GenerateMap();
     }
 
     public void GenerateMap() {
-        map = new int[width, height];
+        Color32[] pixelColors = mapSprite.GetPixels32();
+
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                SpawnTile(pixelColors[(y * width) + x], x, y);
+            }
+        }
+        Debug.Log("done");
+    }
+
+    void SpawnTile(Color32 color, int x, int y) {
+        if(spriteDictionary.ContainsKey(color)){
+            GameObject[] spriteArray = spriteDictionary[color];
+            int idx = 0;
+            if(spriteArray.Length > 1) {
+                idx = UnityEngine.Random.Range(0, spriteArray.Length);
+            }
+            GameObject tilePrefab = spriteArray[idx];
+            Vector2 tilePosition = new Vector2(-width / 2 + 0.5f + x, -height / 2 + 0.5f + y);
+            GameObject tileSprite = Instantiate(tilePrefab, tilePosition, Quaternion.identity) as GameObject;
+        }
+        else {
+            Debug.LogError("No sprite for color: " + color.ToString());
+        }
+
+
     }
 }
