92a9ea0de37090c8c6ac86ab0043d229be0552cf
James Khan <jamesd.khan@mail.utoronto.ca>
2017-10-29 15:31:59 -0400

    Assets imported

---
+++ BassDefense/Assets/Scripts/Controllers/ClassicalAudioController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassicalAudioController : MonoBehaviour {
+    public AudioSource[] sources;
+    public static int activebags;
+    public static int activeharps;
+    public static int activetrumpets;
+    public static int activepianos;
+    float time;
+    float timeint;
+    bool started;
+	// Use this for initialization
+	void Start () {
+        started = false;
+        activebags = 0;
+        activeharps = 0;
+        activetrumpets = 0;
+        activepianos = 0;
+        time = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeint = Time.time - time;
+        
+        
+	}
+
+   
+
+}
