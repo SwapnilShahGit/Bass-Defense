7259be13ae34299ec272ac848aa3f5fa21527e38
James Khan <jamesd.khan@mail.utoronto.ca>
2017-11-06 17:59:18 -0500

    Overflow ability

---
+++ BassDefense/Assets/Scripts/Towers/OverflowBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverflowBehaviour : MonoBehaviour
+{
+    public float speed;
+    public Vector3 target;
+    int dmg = 100;
+    // Use this for initialization
+    void Start()
+    {
+        speed = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (target != null)
+        {
+            this.gameObject.transform.position = Vector2.MoveTowards(this.gameObject.transform.position, target, speed * Time.deltaTime);
+ 
+        }
+
+            if (Vector3.Distance(this.transform.position, target) == 0)
+            {
+                Collider2D[] enemies = Physics2D.OverlapCircleAll(target,10);
+                foreach (Collider2D enemycol in enemies){
+                    GameObject e = enemycol.gameObject;
+                    if (e.GetComponent<EnemyController>() != null){
+                        e.GetComponent<EnemyController>().hp -= dmg;
+                    }
+                }
+                Destroy(this.gameObject);
+            }
+
+    }
+}
